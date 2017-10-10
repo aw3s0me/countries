e3713258bb809b318fb1db3d906d2a0b012fa86e
Aleksandr Korovin <aleksandr.korovin@fit.fraunhofer.de>
2017-10-10 19:24:34 +0200

    gradle wrapper

--- src/main/kotlin/de/korovin/countries/Main.kt
+++ src/main/kotlin/de/korovin/countries/Main.kt
@@ -1,8 +1,16 @@
 package de.korovin.countries
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+@EnableAutoConfiguration
+@Configuration
+class Application
+
 /**
  * Created by aw3s0 on 10/10/2017.
-1 */
+ */
 fun main(args: Array<String>) {
-    println("dd")
+    SpringApplication.run(Application::class.java, *args)
 }
