641d56e93e768a3b0b7054bf69a139feabf58f6d
Aleksandr Korovin <aleksandr.korovin@fit.fraunhofer.de>
2017-10-12 17:14:49 +0200

    sample datatable data

--- frontend/src/app/countries/countries.module.ts
+++ frontend/src/app/countries/countries.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CountriesComponent } from './countries.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   imports: [
+    NgxDatatableModule,
     CommonModule
   ],
   declarations: [CountriesComponent]
